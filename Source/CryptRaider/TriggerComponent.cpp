7171d6583b924c3f8095d38d91bb832aa7d4e157
Utku ERDEN <31778258+Fisixus@users.noreply.github.com>
2022-10-01 16:45:41 -0400

    Overlapping and triggering controls are added

---
+++ Source/CryptRaider/TriggerComponent.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TriggerComponent.h"
+
+UTriggerComponent::UTriggerComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+// Called when the game starts
+void UTriggerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	// ...
+	
+}
+
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+	FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	TArray<AActor*> OverlappingActors;
+	GetOverlappingActors(OverlappingActors);
+	if(OverlappingActors.Num() > 0)
+	{
+		for (auto OverlappingActor : OverlappingActors)
+		{
+			if(OverlappingActor->ActorHasTag(*AcceptableTagName))
+			{
+				UE_LOG(LogTemp, Display, TEXT("%s"),*(OverlappingActor->GetActorNameOrLabel()));
+			}
+		}
+	}
+}
